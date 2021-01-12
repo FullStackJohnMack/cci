4f25a2f8be4168c439a8c271c0d463c66b91ef5c
John Mack <mackspace@gmail.com>
2021-01-11 22:41:33 -0700

    cci #2

--- problems.js
+++ problems.js
@@ -37,4 +37,29 @@ function oneSubOptimal(string) {
 
 // console.log(oneSubOptimal("asdfgjklmnbvcxzqwertyuiopp."));
 
+/**
+ * 2. Check Permutation
+ * Given two strings, write a method to decide if one is a permutation of the other
+ */
 
+function permutation(str1, str2) {
+    if (str1 === str2) return true; //O(1) where two strings are the same
+
+    if (str1.length !== str2.length) return false; //O(1) where different lengths
+
+    const obj = {};
+
+    //O(N) runtime
+
+    for (let char of str1) {
+        obj[char] = 1;
+    }
+
+    for (let char of str2) {
+        if (!obj[char]) return false;
+    }
+
+    return true;
+}
+
+console.log(permutation("amanaplanacanalpanama", "acanalpanamaamanaplan"));
